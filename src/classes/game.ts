9f77de6ebd37ebeeff7d968f609319e845214d48
jacknkandy <jacknkandy@gmail.com>
2022-02-23 03:07:06 +1100

    Include dead state for sprites; Add second sprite; Simulate zombies following the players position; Correctly sort the sprites based on their y position; Check if zombie sprites collide with the player

--- src/classes/game.ts
+++ src/classes/game.ts
@@ -8,29 +8,26 @@ export class Game {
     private app: PIXI.Application = null!;
     private input: Input = null!;
     private player: Player = null!;
-    
 
-    gameLoop: ((delta: number) => void) = (delta: number) => {
-        if (this.player && this.input) {
-            const player = this.player;
-            player.movingForward = this.input.getKeyState(87) || false; // w
-            player.movingBackward = this.input.getKeyState(83) || false; // d
-            player.movingLeft = this.input.getKeyState(65) || false; // a
-            player.movingRight = this.input.getKeyState(68) || false; // d
-            player.update(delta);
-        }
-    };
+    private zombies: Player[] = [];
+
+    private readonly width = 800;
+    private readonly height = 600;
 
     constructor() {
+        PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
         // App
         const app = new PIXI.Application({
-            width: 800,
-            height: 600,
+            width: this.width,
+            height: this.height,
             backgroundColor: 0xff00ff,
             resolution: window.devicePixelRatio || 1
         });
 
         app.stage.addChild(new PIXI.Container());
+        app.stage.scale.set(2);
+
         this.app = app;
 
         // Input
@@ -49,7 +46,9 @@ export class Game {
         this.watchGameEvents();
 
         if (this.app) {
-            this.app.ticker.add(this.gameLoop);
+            this.app.ticker.add((delta: number) => {
+                this.gameLoop(delta);
+            });
         }
     }
 
@@ -58,7 +57,9 @@ export class Game {
             // Load first girl character sprite
             this.app.loader
                 .add('girl1', '/assets/girl1.json')
+                .add('girl2', '/assets/girl2.json')
                 .load((_, r) => {
+                    // Player
                     if (r && r['girl1']) {
                         const res = r['girl1'];
                         const frameNames = res.data && res.data.frames ? Object.keys(res.data.frames) : [];
@@ -68,17 +69,109 @@ export class Game {
             
                         this.app.stage.addChild(p.getSprite());
                     }
+
+                    // Zombies
+                    if (r && r['girl2']) {
+                        const res = r['girl2'];
+                        const frameNames = res.data && res.data.frames ? Object.keys(res.data.frames) : [];
+
+                        for (let i = 0; i < 10; i++) {
+                            const x = this.app.stage.width * Math.random();
+                            const y = this.app.stage.height * Math.random();
+
+                            const z = new Player(frameNames);
+                            z.setPosition(x, y);
+                            z.setZombie(true);
+                            this.app.stage.addChild(z.getSprite());
+
+                            this.zombies.push(z);
+                        }
+                    }                    
                 });
         }
     }
 
-    private watchGameEvents() {
+    private watchGameEvents(): void {
         // Toggle the zombie mode when the user presses space
         this.input.onKeyPress(32)
             .subscribe(() => {
-                this.player.setZombie(!this.player.getZombie());
+                const z = this.player.getZombie();
+                const d = this.player.getIsDead();
+
+                if (!z && !d) {
+                    this.player.setZombie(true);
+                } else if (z && !d) {
+                    this.player.setZombie(false);
+                    this.player.setIsDead(true);
+                } else if (!z && d) {
+                    this.player.setIsDead(false);
+                }
             })
     }
 
+    private gameLoop(delta: number): void {
+        if (this.player && this.input) {
+            const player = this.player;
+            player.setIsMovingForward(this.input.getKeyState(87) || false); // w
+            player.setIsMovingBackward(this.input.getKeyState(83) || false); // d
+            player.setIsMovingLeft(this.input.getKeyState(65) || false); // a
+            player.setIsMovingRight(this.input.getKeyState(68) || false); // d
+            player.update(delta);
+            
+            const pos = player.getPosition();
+
+            // TO DO - Boids to scatter
+            for (const zombie of this.zombies) {
+                zombie.followPosition(pos[0], pos[1]);
+                zombie.update(delta);
+            }
+            
+        }
+
+        this.updateZSorting();
+        this.checkCollisions();
+    }
+
+    private updateZSorting(): void {
+        // Update order on the stage based on current y position (based on anchor)
+        // If they are dead they stay on the ground (need more configuration here)
+        this.app.stage.children.sort((a, b) => {
+            if(a.y < b.y) {
+                return -1;
+            } else if (a.y > b.y) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+    }
+
+    private doIntersect(a: PIXI.Rectangle, b: PIXI.Rectangle): boolean {
+        return a.x + a.width > b.x &&
+            a.x < b.x + b.width &&
+            a.y + a.height > b.y &&
+            a.y < b.y + b.height;
+    }
+    
+    private checkCollisions(): void {
+        // Check if player and zombies collide
+        if (this.player && this.zombies && this.zombies.length > 0) {
+            const player = this.player;
+            const playerBounds = player.getBounds();
+    
+            const zombies = this.zombies;
+    
+            for (const zombie of zombies) {
+                const zombBound = zombie.getBounds();
+    
+                const interesct = this.doIntersect(playerBounds, zombBound);
+
+                if (interesct) {
+                    console.log('intersects', interesct);
+                }
+                
+            }
+        }
+    }
     
 }
--- src/classes/player.ts
+++ src/classes/player.ts
@@ -8,23 +8,25 @@ interface PlayerStateTextures {
 }
 
 export class Player {
-    x: number = 0;
-    y: number = 0;
+    private prevDirection: [number, number] = [0, 0];
 
     private readonly anchor: number = 0.5;
     private readonly animationSpeed: number = 0.1;
     private readonly speed: number = 1;
     private readonly zombieSpeed: number = 0.5;
 
-    private prevIsMoving = false;
+    private isMoving: boolean = false;
+    private prevIsMoving: boolean = false;
 
+    private isDead: boolean = false;
     private isZombie: boolean = false;
     
-    movingForward = false;
-    movingBackward = false;
-    movingLeft = false;
-    movingRight = false;
+    private movingForward = false;
+    private movingBackward = false;
+    private movingLeft = false;
+    private movingRight = false;
 
+    private deadTextures: PIXI.Texture[];
     private textures: PlayerStateTextures;
     private zombieTextures: PlayerStateTextures;
 
@@ -35,29 +37,32 @@ export class Player {
             return PIXI.Texture.from(n);
         });
 
-        if (!frames || frames.length !== 24) console.error('Invalid frame count');
+        if (!frames || frames.length !== 26) console.error('Invalid frame count');
 
         this.isZombie = !!isZombie;
 
+        // Dead textures
+        this.deadTextures = frames.slice(0, 2);
+
         // Normal character textures
         this.textures = {
-            forward: frames.slice(0, 3),
-            left: frames.slice(3, 6),
-            right: frames.slice(6, 9),
-            backward: frames.slice(9, 12)
+            forward: frames.slice(2, 5),
+            left: frames.slice(5, 8),
+            right: frames.slice(8, 11),
+            backward: frames.slice(11, 14)
         };
 
         // Zombies textures
         this.zombieTextures = {
-            forward: frames.slice(12, 15),
-            left: frames.slice(15, 18),
-            right: frames.slice(18, 21),
-            backward: frames.slice(21, 24)
+            forward: frames.slice(14, 17),
+            left: frames.slice(17, 20),
+            right: frames.slice(20, 23),
+            backward: frames.slice(23, 26)
         };
 
         const sprite = new PIXI.AnimatedSprite(this.isZombie ? this.zombieTextures.forward : this.textures.forward);
-        sprite.x = 20;
-        sprite.y = 20;
+        sprite.x = 0;
+        sprite.y = 0;
         sprite.anchor.set(this.anchor);
         sprite.animationSpeed = this.animationSpeed;
         sprite.gotoAndStop(1);
@@ -69,13 +74,9 @@ export class Player {
         return this.sprite;
     }
 
-    update(delta: number): void {
-        const x = Number(this.movingRight) - Number(this.movingLeft);
-        const y = Number(this.movingForward) - Number(this.movingBackward);
+    getStateTexture(x: number, y: number): PIXI.Texture[] | PIXI.FrameObject[] {
+        if (this.isDead) return this.deadTextures;
 
-        const isMoving = (x !== 0) || (y !== 0);
-
-        // Update texture frames based on direction character is facing
         let textures = this.sprite.textures;
 
         if (y < 0) {
@@ -88,20 +89,38 @@ export class Player {
             textures = this.isZombie ? this.zombieTextures.left : this.textures.left;
         } else if (x > 0) {
             textures = this.isZombie ? this.zombieTextures.right : this.textures.right;
-        }        
+        }
+
+        return textures;
+    }
+
+    update(delta: number): void {
+        if (this.isDead) return;
+
+        const x = Number(this.movingRight) - Number(this.movingLeft);
+        const y = Number(this.movingForward) - Number(this.movingBackward);
+
+        const isMoving = (x !== 0) || (y !== 0);
+
+        // Update texture frames based on direction character is facing
+        const textures = this.getStateTexture(x, y);
+
+        let textureChanged = false;
 
         if (this.sprite.textures !== textures) {
             this.sprite.textures = textures;
-            this.sprite.play();
+            textureChanged = true;
         }
 
-        if (!this.prevIsMoving && isMoving) {
+        if ((!this.prevIsMoving && isMoving) || textureChanged) {
+            this.prevDirection = [x, y];
             this.sprite.play();
         } else if (this.prevIsMoving && !isMoving) {
             this.sprite.gotoAndStop(1);
         }
 
-        this.prevIsMoving = isMoving;  
+        this.prevIsMoving = this.isMoving;  
+        this.isMoving = isMoving;
 
         const speed = (this.isZombie ? this.zombieSpeed : this.speed)
         this.sprite.x += speed * x * delta;
@@ -110,9 +129,90 @@ export class Player {
 
     setZombie(isZombie: boolean): void {
         this.isZombie = isZombie;
+
+        const textures = this.getStateTexture(this.prevDirection[0], this.prevDirection[1]);
+
+        if (this.sprite.textures !== textures) {
+            this.sprite.textures = textures;
+        }
+
+        if (this.isMoving) {
+            this.sprite.play();
+        } else {
+            this.sprite.gotoAndStop(1);
+        }
     }
 
     getZombie(): boolean {
         return this.isZombie;
+    }
+
+    setIsDead(isDead: boolean): void {
+        this.isDead = isDead;
+
+        const textures = this.getStateTexture(this.prevDirection[0], this.prevDirection[1]);
+
+        if (this.sprite.textures !== textures) {
+            this.sprite.textures = textures;
+        }
+
+        if (this.isMoving && !isDead) {
+            this.sprite.play();
+        } else {
+            this.sprite.gotoAndStop(1);
+        }
+    }
+
+    getIsDead(): boolean {
+        return this.isDead;
+    }
+
+    setPosition(x: number, y: number): void {
+        this.sprite.x = x;
+        this.sprite.y = y;
+    }
+
+    getPosition(): [number, number] {
+        return [this.sprite.x, this.sprite.y];
+    }
+
+    setIsMovingForward(forward: boolean): void {
+        this.movingForward = forward;
+    }
+
+    setIsMovingBackward(backward: boolean): void {
+        this.movingBackward = backward;
+    }
+
+    setIsMovingLeft(left: boolean): void {
+        this.movingLeft = left;
+    }
+
+    setIsMovingRight(right: boolean): void {
+        this.movingRight = right;
+    }
+
+    setZIndex(zIndex: number): void {
+        if (this.sprite) {
+            this.sprite.zIndex = zIndex;
+        }
+    }
+
+    followPosition(targetX: number, targetY: number): void {
+        const x = this.sprite.x;
+        const y = this.sprite.y;
+
+        const dx = targetX - x;
+        const dy = targetY - y;
+
+        this.movingLeft = (Math.ceil(dx) < 0);
+        this.movingRight = (Math.ceil(dx) > 0);
+
+        this.movingForward = (Math.ceil(dy) < 0);
+        this.movingBackward = (Math.ceil(dy) > 0);
+    }
+
+    getBounds(): PIXI.Rectangle {
+        return this.sprite.getBounds();
     }
 }
