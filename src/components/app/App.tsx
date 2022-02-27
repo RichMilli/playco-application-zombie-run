1f2db987a639bfc7ab52919425382ff169f8fb3d
jacknkandy <jacknkandy@gmail.com>
2022-02-27 20:27:04 +1100

    Added instructions and link to main page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <PixiContainer/>
+      <div className="how-to">
+        How to play: 
+        <br/><br />
+        Use the WASD keys to move. <br/>
+        Try and save as many hospital staff from becoming zombies and get the highest score.
+        <br /><br />
+        See at GitHub: <a href="https://github.com/jacknkandy/playco-application-zombie-run">https://github.com/jacknkandy/playco-application-zombie-run</a>
+      </div>
     </div>
   );
 }
